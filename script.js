4d81ceb229d78067ccb57bb9cea61503090e36f1
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2020-12-21 12:19:49 -0600

    Created getLength and getInfo Functions

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Functino to get character length information
+function getLength() {
+var length = prompt("Character length of password?");
+
+  if (length < 8) {
+    alert("Please choose a number between 8 and 128");
+    var length = prompt("Character length of password?");
+    return length;
+  } else if (length > 128) {
+    alert("Please choose a number between 8 and 128");
+    var length = prompt("Character length of password?");
+    return length;
+  } else {
+    return length;
+  }
+}
+
+//Function to begin functions on clicking the button
+generateBtn.onclick = function getInfo() {
+  getLength();
+};
+
+//Function to generate password.
+function generatePassword (){
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
