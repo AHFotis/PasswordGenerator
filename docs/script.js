22579d493b42814082fe3c3c763e649ff2e74433
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2020-12-28 17:12:31 -0600

    Clean Up JS Code

--- docs/script.js
+++ docs/script.js
@@ -2,23 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // Global Variables
-var openA = [];
+// var openA = [];
+// var charLength = "";
 
 //Function to get length
 function getLength() {
 
-  var charLength = prompt("Character length of password (between 8-128)?");
-  charLength = parseInt(charLength);
+  var length = prompt("Character length of password (between 8-128)?");
+  length = parseInt(length);
 
-  while (charLength < 8 || charLength > 128 || isNaN(charLength)) {
+  while (length < 8 || length > 128 || isNaN(length)) {
     alert("Please choose and input a number between 8 and 128.");
-    charLength = prompt("Character length of password (between 8-128)?");
-    charLength = parseInt(charLength);
+    length = prompt("Character length of password (between 8-128)?");
+    length = parseInt(length);
   }
 
-  alert("You have selected " + charLength + " characters.")
-  console.log(charLength);
-  return charLength;
+  alert("You have selected " + length + " characters.")
+  console.log(length);
+  return length;
 }
 
 //Function to get booleans
@@ -29,42 +30,46 @@ function getTrueFalse() {
   var numericChar = "";
   var specialChar = "";
 
+  alert("Let's choose your characters! You must choose at least one type.");
+
   //Wan lower case?
   lowerCase = confirm("Include LOWER CASE characters?");
 
-  if (lowerCase) {
-    alert("You have confirmed LOWER CASE characters.");
-  } else {
-    alert("No LOWER CASE characters");
-  }
+    if (lowerCase) {
+      alert("You have confirmed LOWER CASE characters.");
+    } else {
+      alert("No LOWER CASE characters");
+    }
+
   //Want uppercase?
   upperCase = confirm("Include UPPER CASE characters?");
 
-  if (upperCase) {
-    alert("You have confirmed UPPER CASE characters.");
-  } else {
-    alert("No UPPER CASE characters");
-  }
+    if (upperCase) {
+      alert("You have confirmed UPPER CASE characters.");
+    } else {
+      alert("No UPPER CASE characters");
+    }
+
   //Want numeric?
   numericChar = confirm("Include NUMERIC characters?");
 
-  if (numericChar) {
-    alert("You have confirmed NUMERIC characters.");
-  } else {
-    alert("No NUMERIC characters");
-  }
+    if (numericChar) {
+      alert("You have confirmed NUMERIC characters.");
+    } else {
+      alert("No NUMERIC characters");
+    }
 
   specialChar = confirm("Include SPECIAL characters?");
 
-  if (specialChar) {
-    alert("You have confirmed SPECIAL characters.");
-  } else {
-    alert("No SPECIAL characters");
-  }
+    if (specialChar) {
+      alert("You have confirmed SPECIAL characters.");
+    } else {
+      alert("No SPECIAL characters");
+    }
 
   //Boolean while loop
   while (!lowerCase && !upperCase && !numericChar && !specialChar) {
-    alert("Must chose at least one type of character. Please answer again.");
+    alert("You must chose at least one type of character. Please answer again.");
     lowerCase = confirm("Include LOWER CASE characters?");
     if (lowerCase) {
       alert("You have confirmed LOWER CASE characters.");
@@ -100,13 +105,15 @@ function getTrueFalse() {
 
 }
 
-// Function to run functions when button is clicked
-generateBtn.onclick = function generatePassword() {
+// Function to run previous functions and return usable array
+generateBtn.onclick = function getArray() {
 
-  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var special = ["!", "?", "{", "}", "|", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "[", "]", "=", ":", ";", "/", "-"];
+var characters = {
+  upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  lower:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  special: ["!", "?", "{", "}", "|", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "[", "]", "=", ":", ";", "/", "-"],
+}
 
   // Declared here to reset if Generate Password is pushed again without refreshing.
   openA = [];
@@ -119,16 +126,16 @@ generateBtn.onclick = function generatePassword() {
   console.log(trueFalse);
 
   if (trueFalse[0] === true) {
-    openA = openA.concat(lower);
+    openA = openA.concat(characters.lower);
   }
   if (trueFalse[1] === true) {
-    openA = openA.concat(upper);
+    openA = openA.concat(characters.upper);
   }
   if (trueFalse[2] === true) {
-    openA = openA.concat(number);
+    openA = openA.concat(characters.number);
   }
   if (trueFalse[3] === true) {
-    openA = openA.concat(special);
+    openA = openA.concat(characters.special);
   }
 
   console.log(openA);
